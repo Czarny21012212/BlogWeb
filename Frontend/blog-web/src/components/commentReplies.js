c10b89a6ec850c6e9d21619e0444b776854a3003
Wiktor <wiktorstachula20@gmail.com>
2025-05-09 19:15:38 +0200

    add functio to reply comment

---
+++ Frontend/blog-web/src/components/commentReplies.js
@@ -0,0 +1,175 @@
+import React, { use, useEffect, useState } from 'react';
+import '../Home/home.css';
+
+
+function CommentsReplies({ CommentId }) {
+
+    const commentData = {
+        "comment_id": CommentId
+    }
+
+    const [commentsRepliesVisible, setCommentsRepliesVisible] = useState(false);
+
+
+    const [replies, setReplies] = React.useState([]);
+    
+
+    useEffect(() => {
+        fetch("http://localhost:8082/api/show-comments-replies", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(commentData)
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Failed to fetch comments');
+            }
+        })
+        .then(data => {
+            setReplies(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+    }, []);
+
+    const changeVisibility = () => {
+        setCommentsRepliesVisible(!commentsRepliesVisible);
+    }
+
+    const changeDateFormat = (dateString) => {
+        const date = new Date(dateString);
+        const NowDate = new Date();
+        const betweenDates = Math.floor((NowDate - date) / 1000 );
+
+        if(betweenDates < 3600) {
+          const minutes = Math.floor(betweenDates / 60);
+          return `${minutes} minuts ago`;  
+        }else if(betweenDates > 3600 && betweenDates < 86400) {
+            const hours = Math.floor(betweenDates / 3600);
+            return `${hours} hours ago`;
+        }else if(betweenDates > 86400 && betweenDates < 2592000) {
+            const days = Math.floor(betweenDates / 86400);
+            return `${days} days ago`;
+        }
+        else if(betweenDates > 2592000 && betweenDates < 31536000) {
+            const months = Math.floor(betweenDates / 2592000);
+            return `${months} months ago`;
+        }else {
+            const years = Math.floor(betweenDates / 31536000);
+            return `${years} years ago`;
+        }
+    }   
+
+
+
+
+
+
+    const [yourCommentReply, setYourCommentReply] = useState({
+        content: '',
+        publicationDate: new Date().toISOString(),
+        comment_id: CommentId
+      });
+    
+    const [commentMessageReply, setCommentMessageReply] = useState('');
+  
+      const checkCommentReply = () => {
+        
+  
+        const userCommentReply = yourCommentReply.content.trim();
+        console.log(yourCommentReply)
+  
+        if (userCommentReply === '') {
+            setCommentMessageReply('Please enter a comment');
+            return;
+        }
+  
+        const sanitizeInput = (input) => {
+          
+            return input.replace(/</g, "&lt;").replace(/>/g, "&gt;")
+                        .replace(/&/g, "&amp;").replace(/"/g, "&quot;")
+                        .replace(/'/g, "&#x27;");
+            };
+  
+            const validateComment = (comment) => {
+                const forbiddenPatterns = [/script/i, /<.*?>/g, /on\w+=/g];
+                return !forbiddenPatterns.some(pattern => pattern.test(comment));
+            };
+  
+            const sanitizedComment = sanitizeInput(userCommentReply);
+  
+            if (!validateComment(sanitizedComment)) {
+                setCommentMessageReply('Invalid comment. Please remove any prohibited characters.');
+                return;
+            }  
+            sendCommentReply()
+        };
+  
+        const sendCommentReply = () => {
+          fetch("http://localhost:8082/api/comments-replies", {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              credentials: 'include',
+              body: JSON.stringify(yourCommentReply)
+          })
+          .then(response => {
+              if (response.ok) {
+                  return response.json();
+              } else {
+                  throw new Error('Failed to add comment');
+              }
+          })
+          .then(data => {
+                setReplies([...replies, data]);
+                setYourCommentReply({ content: '', publicationDate: new Date().toISOString() });
+          })
+          .catch(error => {
+              console.error('Error:', error);
+          });
+        }
+
+
+
+
+
+
+
+
+  return (
+   <>
+    <div>
+        
+        {replies.length != 0 && <p onClick={changeVisibility} className='reply-show'>view {replies.length} replies</p>}
+        {commentsRepliesVisible && (
+        <div>
+            <p>write reply</p>
+            <input type="text" placeholder="Your reply" onChange={e => setYourCommentReply({...yourCommentReply, content: e.target.value})}/>
+            <input type="submit" onClick={checkCommentReply}/>
+
+            {replies.map((reply) => (
+                <div className="reply" key={reply.id}>
+                    <div className="reply-header">
+                        <span className="reply-author">@{reply.userName}</span>
+                        <span className="reply-publication">{changeDateFormat(reply.publicationDate)}</span>  
+                    </div>
+                    <p className="reply-content">{reply.content}</p>
+                </div>
+            ))}
+        </div>
+)}
+       
+    </div>
+   </>
+  );
+}
+
+export default CommentsReplies;
--- Frontend/blog-web/src/components/comments.js
+++ Frontend/blog-web/src/components/comments.js
@@ -1,6 +1,7 @@
 import  { use, useEffect, useState } from 'react';
 import { Send } from 'lucide-react';
 import '../Home/home.css';
+import CommentsReplies from './commentReplies';
 
 function Comments({ postId }) {
 
@@ -54,6 +55,7 @@ function Comments({ postId }) {
   
 
     const checkComment = () => {
+      
 
       const userComment = yourComment.content.trim();
       console.log(yourComment)
@@ -64,6 +66,7 @@ function Comments({ postId }) {
       }
 
       const sanitizeInput = (input) => {
+        
           return input.replace(/</g, "&lt;").replace(/>/g, "&gt;")
                       .replace(/&/g, "&amp;").replace(/"/g, "&quot;")
                       .replace(/'/g, "&#x27;");
@@ -108,6 +111,38 @@ function Comments({ postId }) {
         });
       }
 
+      const changeDateFormat = (dateString) => {
+        const date = new Date(dateString);
+        const NowDate = new Date();
+        const betweenDates = Math.floor((NowDate - date) / 1000 );
+
+        if(betweenDates < 3600) {
+          const minutes = Math.floor(betweenDates / 60);
+          if (minutes === 0) {
+            return `Now`;
+          }else if (minutes === 1) {
+            return `1 minute ago`;
+          }
+          return `${minutes} minuts ago`;  
+        }else if(betweenDates > 3600 && betweenDates < 86400) {
+            const hours = Math.floor(betweenDates / 3600);
+            return `${hours} hours ago`;
+        }else if(betweenDates > 86400 && betweenDates < 2592000) {
+            const days = Math.floor(betweenDates / 86400);
+            return `${days} days ago`;
+        }
+        else if(betweenDates > 2592000 && betweenDates < 31536000) {
+            const months = Math.floor(betweenDates / 2592000);
+            return `${months} months ago`;
+        }else {
+            const years = Math.floor(betweenDates / 31536000);
+            return `${years} years ago`;
+        }
+    }   
+
+    const taggle = (e) => {
+        setYourComment({ ...yourComment, [e.target.name]: e.target.value });   
+    }
     return (
         <div className="comments-container">
           <p className="toggle-comments" onClick={handleShowComments}>
@@ -115,31 +150,18 @@ function Comments({ postId }) {
           </p>
     
           {postVisible && (
+            
             <div>
-              <div className="comments-divider" />
-              {comments.length > 0 ? (
-                comments.map((comment) => (
-                  <div className="comment" key={comment.id}>
-                    <div className="comment-header">
-                      <span className="comment-author">@{comment.userName}</span>
-                      <span className="comment-date">{comment.publicationDate}</span>
-                    </div>
-                    <p className="comment-content">{comment.content}</p>
-
-                  </div>
-                ))
-              ) : (
-                <p className="no-comments">Brak komentarzy</p>
-              )}
-              <div className="comment-form">
+               <div className="comment-form">
                 <div className="comment-input-wrapper">
                   <input
                     id="comment-content"
                     type="text"
                     placeholder="Wpisz swój komentarz..."
                     value={yourComment.content}
-                    maxLength={80}
-                    onChange={e => setYourComment({ ...yourComment, content: e.target.value })}
+                    maxLength={78}
+                    name = "content"
+                    onChange={taggle}
                     className="comment-input"
                   />
                   <button type="button" onClick={checkComment} className="comment-submit">
@@ -148,8 +170,21 @@ function Comments({ postId }) {
                 </div>
                 {commentMessage && <p className="comment-error">{commentMessage}</p>}
               </div>
-
-
+              <div className="comments-divider" />
+              {comments.length > 0 ? (
+                comments.map((comment) => (
+                  <div className="comment" key={comment.id}>
+                    <div className="comment-header">
+                      <span className="comment-author">@{comment.userName}</span>
+                      <span className="comment-date">{changeDateFormat(comment.publicationDate)}</span>
+                    </div>
+                    <p className="comment-content">{comment.content}</p>
+                    <CommentsReplies CommentId={comment.comment_Id} />
+                  </div>
+                ))
+              ) : (
+                <p className="no-comments">Brak komentarzy</p>
+              )}
             </div>
           )}
           
